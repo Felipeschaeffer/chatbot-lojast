b71f22a6c1c608407b48265739462471d8788912
Felipeschaeffer <108107338+Felipeschaeffer@users.noreply.github.com>
2025-05-19 22:10:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import { enviarMensagem } from './services/pagebot.js';
-import { enviarPerguntaParaIA } from './services/openai.js';
-
-dotenv.config();
-const app = express();
-app.use(bodyParser.json());
-
-app.post('/webhook', async (req, res) => {
-  const { mensagem, cliente } = req.body;
-  console.log('Mensagem recebida:', mensagem);
-
-  const resposta = await enviarPerguntaParaIA(mensagem);
-
-  await enviarMensagem(cliente.id, resposta);
-
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+{
+  "name": "chatbot-loja-st",
+  "version": "1.0.1",
+  "description": "Chatbot para loja virtual utilizando Express e integração com WhatsApp",
+  "author": "Felipe",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js",
+    "test": "jest",
+    "lint": "eslint .",
+    "format": "prettier --write ."
+  },
+  "dependencies": {
+    "axios": "^1.6.8",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.0",
+    "eslint": "^8.58.0",
+    "prettier": "^3.2.5",
+    "jest": "^30.1.0"
+  },
+  "license": "MIT"
+}
