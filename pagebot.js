ea734b38133cf5a9b4f1b6adee0c8f363538dc2a
Felipeschaeffer <108107338+Felipeschaeffer@users.noreply.github.com>
2025-05-19 22:08:10 -0300

    Update pagebot.js

--- pagebot.js
+++ pagebot.js
@@ -1,12 +1,28 @@
-import axios from 'axios';
-
-export const enviarMensagem = async (clienteId, texto) => {
-  await axios.post('https://api.pagebot.com.br/v1/mensagens', {
-    cliente_id: clienteId,
-    mensagem: texto
-  }, {
-    headers: {
-      Authorization: `Bearer ${process.env.PAGEBOT_API_KEY}`
-    }
-  });
-};
+{
+  "name": "chatbot-loja-st",
+  "version": "1.0.1",
+  "description": "Chatbot para loja virtual utilizando Express",
+  "author": "Felipe",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js",
+    "test": "echo \"Nenhum teste configurado\" && exit 0",
+    "lint": "eslint .",
+    "format": "prettier --write .",
+    "build": "echo \"Nenhum processo de build configurado\""
+  },
+  "dependencies": {
+    "axios": "^1.6.8",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.0",
+    "eslint": "^8.58.0",
+    "prettier": "^3.2.5"
+  },
+  "license": "MIT"
+}
